641e757e8ec3ffd136297e36e72d3109d55d3649
Bailey Morris <bailey.morris71@gmail.com>
2019-09-26 09:08:16 -0500

    bailey

--- recursionLearn/Program.cs
+++ recursionLearn/Program.cs
@@ -5,23 +5,24 @@ namespace recursionLearn
     class Program
     {
 
-        static void Main(string[] args)
+        static void Main(string[] args)                 //A recursion is when you call the method inside of itself forming a type of loop
         {
 
-            Console.WriteLine(Factorial(5));
+            Console.WriteLine(Factorial(5));            //printing the factorial of 5 and 10 on lines 11 and 12
             Console.WriteLine(Factorial(10));
+            Console.WriteLine(baileyBranch.Baloo(15));              //calling method and printing the result
 
         }
 
-        static int Factorial(int n)
+        static int Factorial(int n)                     //Creating the method Factorial
         {
-            if (n == 0)
+            if (n == 0)                                //creating if loop where if n equals 0 returns 1
             {
                 return 1;
             }
 
             // recursive solution (as opposed to iterative)
-            return n * Factorial(n - 1);
+            return n * Factorial(n - 1);                //using a recursive by calling the Method factorial inside of itself
 
             // e.g.
             // 5! = 5 * 4!
@@ -31,5 +32,6 @@ namespace recursionLearn
             //     1! = 1 * 0!
             //      0! = 1 (because math)
         }
+        
     }
 }
---
+++ recursionLearn/baileyBranch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace recursionLearn
+{
+    public class baileyBranch
+    {
+        public static int Baloo(int raj)                      //this is a simple method that will add every number from the first number down to zero
+        {
+            if (raj == 0)
+                return 1;
+            return raj + Baloo(raj - 1);               //returning raj plus the method Baloo with raj minus 1 in the paremeters
+
+        }
+    }
+}
